de6b619e5ce94af7a0ae5b75b20a1c84e95c0f63
Frédéric Douville <frederic.douville@zoho.eu>
2023-03-04 15:14:08 +0100

    Ajout composant physical variable

--- src/components/physical variable/index.jsx
+++ src/components/physical variable/index.jsx
@@ -1,5 +1,64 @@
-function PhysicalVariable() {
-    return <div></div>;
+import { ReactComponent as CircleDotIcon } from '../../assets/icones/circle-dot-regular.svg';
+import { ReactComponent as CubeIcon } from '../../assets/icones/cube-solid.svg';
+import { ReactComponent as GemIcon } from '../../assets/icones/gem-solid.svg';
+import { ReactComponent as GlobeIcon } from '../../assets/icones/globe-solid.svg';
+import { ReactComponent as GravityIcon } from '../../assets/icones/gravity.svg';
+import { ReactComponent as HourglassIcon } from '../../assets/icones/hourglass-half-solid.svg';
+import { ReactComponent as RocketIcon } from '../../assets/icones/rocket-solid.svg';
+import { ReactComponent as RulerIcon } from '../../assets/icones/ruler-solid.svg';
+import { ReactComponent as StopwatchIcon } from '../../assets/icones/stopwatch-solid.svg';
+import { ReactComponent as TemperatureIcon } from '../../assets/icones/temperature-half-solid.svg';
+import { ReactComponent as WeightIcon } from '../../assets/icones/weight-hanging-solid.svg';
+
+function PhysicalVariable({
+    icon,
+    title,
+    data,
+    exp,
+    isExp,
+    unit,
+    unitExp,
+    isUnitExp,
+}) {
+    const iconList = {
+        circle: <CircleDotIcon className="physical-var-icon" />,
+        cube: <CubeIcon className="physical-var-icon" />,
+        gem: <GemIcon className="physical-var-icon" />,
+        globe: <GlobeIcon className="physical-var-icon" />,
+        gravity: <GravityIcon className="physical-var-icon" />,
+        hourglass: <HourglassIcon className="physical-var-icon" />,
+        rocket: <RocketIcon className="physical-var-icon" />,
+        ruler: <RulerIcon className="physical-var-icon" />,
+        stopwatch: <StopwatchIcon className="physical-var-icon" />,
+        temperature: <TemperatureIcon className="physical-var-icon" />,
+        weight: <WeightIcon className="physical-var-icon" />,
+    };
+
+    return (
+        <div className="physical-var">
+            {iconList[icon]}
+            <span className="physical-var-title">{title}:</span>
+            <span className="physical-var-data">
+                {isExp === true ? (
+                    <var>
+                        {data}.10<sup>{exp}</sup>
+                    </var>
+                ) : (
+                    data
+                )}
+            </span>
+            <span className="physical-var-unit">
+                {isUnitExp === true ? (
+                    <var>
+                        {unit}
+                        <sup>{unitExp}</sup>
+                    </var>
+                ) : (
+                    unit
+                )}
+            </span>
+        </div>
+    );
 }
 
 export default PhysicalVariable;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,133 @@
-import { Carousel, Description } from '../../components';
+import { Carousel, Description, PhysicalVariable } from '../../components';
 
 function Home() {
+    const physVarArray = [
+        {
+            icon: 'weight',
+            title: 'Masse',
+            data: '5,98',
+            exp: '24',
+            isExp: true,
+            unit: 'kg',
+            unitExp: '',
+            isUnitExp: false,
+        },
+        {
+            icon: 'cube',
+            title: 'Volume',
+            data: '1,08',
+            exp: '12',
+            isExp: true,
+            unit: 'm',
+            unitExp: '3',
+            isUnitExp: true,
+        },
+        {
+            icon: 'ruler',
+            title: 'Rayon moyen',
+            data: '2500',
+            exp: '',
+            isExp: false,
+            unit: 'km',
+            unitExp: '',
+            isUnitExp: false,
+        },
+        {
+            icon: 'gem',
+            title: 'Densité',
+            data: '5.51',
+            exp: '',
+            isExp: false,
+            unit: 'g.cm',
+            unitExp: '3',
+            isUnitExp: true,
+        },
+        {
+            icon: 'gravity',
+            title: 'Gravité',
+            data: '9.8',
+            exp: '',
+            isExp: false,
+            unit: 'm.s',
+            unitExp: '-2',
+            isUnitExp: true,
+        },
+        {
+            icon: 'rocket',
+            title: "Vitesse d'échappement",
+            data: '11190',
+            exp: '',
+            isExp: false,
+            unit: 'm.s',
+            unitExp: '-1',
+            isUnitExp: true,
+        },
+        {
+            icon: 'temperature',
+            title: 'Température moyenne',
+            data: '20',
+            exp: '',
+            isExp: false,
+            unit: 'K',
+            unitExp: '',
+            isUnitExp: false,
+        },
+        {
+            icon: 'stopwatch',
+            title: 'Période de rotation',
+            data: '24',
+            exp: '',
+            isExp: false,
+            unit: 'heures',
+            unitExp: '',
+            isUnitExp: false,
+        },
+        {
+            icon: 'hourglass',
+            title: 'Période de révolution',
+            data: '365',
+            exp: '',
+            isExp: false,
+            unit: 'Jours terrestres',
+            unitExp: '',
+            isUnitExp: false,
+        },
+    ];
+
     return (
         <div className="home">
             <Carousel />
             <Description />
+            <div className="physical-data-ctn">
+                <div className="physical-data-general">
+                    {physVarArray.map(
+                        ({
+                            icon,
+                            title,
+                            data,
+                            exp,
+                            isExp,
+                            unit,
+                            unitExp,
+                            isUnitExp,
+                        }) => (
+                            <PhysicalVariable
+                                icon={icon}
+                                title={title}
+                                data={data}
+                                exp={exp}
+                                isExp={isExp}
+                                unit={unit}
+                                unitExp={unitExp}
+                                isUnitExp={isUnitExp}
+                                key={`key-${title}`}
+                            />
+                        )
+                    )}
+                </div>
+                <div className="physical-data-incline"></div>
+            </div>
+            <div className="physical-data-orbital"></div>
         </div>
     );
 }
